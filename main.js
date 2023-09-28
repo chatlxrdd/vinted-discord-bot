26c59d44d28faf100d1e212fba06495c1b2ed6da
Your Name <londoyt@gmail.com>
2023-09-28 20:38:50 +0200

    Added error handling

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ const Config = require('./modules/config.js');
 const vintedUrl = Config.vintedLink;
 
 const itemsPerPage = '1'; // ile itemkow ma sie pojawic w API (mniej = szybciej działający kod)
-const catalogIds = ''; // id katalogu, łatwo znalezc wystarczy sprawdzic url na stronie
+const catalogIds = '1206'; // id katalogu, łatwo znalezc wystarczy sprawdzic url na stronie
 const colorIds = ''; // to samo co wyzej
-const brandIds = ''; // jeszcze nie wiem ale sie dowiem ^^
+const brandIds = '2319'; // jeszcze nie wiem ale sie dowiem ^^
 const sizeIds = ''; // to samo co wyzej
 const materialIds = ''; // to samo co wyzej
 const videoGameRatingIds = '';
-const price = '' //   to samo co wyzej
-const currency = ''; // waluta w jakiej ma byc cena (EUR, PLN, USD)
+const price = '120' //   to samo co wyzej
+const currency = 'PLN'; // waluta w jakiej ma byc cena (EUR, PLN, USD)
 const order = 'newest_first'; // sposob w jaki przedmioty sa ukladane w api (newest_first = od najnowszych) (oldest_first = od najstarszych)
 
 const dataUrl = `https://www.vinted.pl/api/v2/catalog/items?per_page=${itemsPerPage}&catalog_ids=${catalogIds}&color_ids=${colorIds}&brand_ids=${brandIds}&size_ids=${sizeIds}&material_ids=${materialIds}&video_game_rating_ids=${videoGameRatingIds}&status_ids&price_to=${price}&currency=${currency}&order=${order}`
@@ -128,4 +128,16 @@ function getNewOffers(cachedOffers, fetchedOffers) {
     const cachedUrls = cachedOffers.map(offer => offer.url);
     const newOffers = fetchedOffers.filter(offer => !cachedUrls.includes(offer.url));
     return newOffers;
+}
+
+function startScraping() {
+    scrape()
+        .then((res) => {
+            discordSendMsg(res);
+        })
+        .catch((error) => {
+            console.error('Error:', error)
+            console.log('Restarting bot...')
+            startScraping() // Restart the bot with the same parameters
+        })
 }
--- modules/config.js
+++ modules/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     refreshTime: 1000,
     discord: {
-        token: "",
-        channelID: "1080884455926935696"
+        token: "MTE1NTI4MDA3NDE5MTQyMTQ5MA.GLtvfn.ju6WFmHrC40xK0cFFn3iKWmyybhJJp_Y1_dgCQ",
+        channelID: "1059837496709611563"
     },
     vintedLink: "https://www.vinted.pl"
 };
