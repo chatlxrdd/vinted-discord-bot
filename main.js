af37010a130f3487c67491801040724e2c7b89f7
Filip Bączkowski <filo.b@wp.pl>
2023-11-27 19:58:51 +0100

    refactor: parse url function

--- main.js
+++ main.js
@@ -6,17 +6,38 @@ const vintedUrl = Config.vintedLink;
 
 function parseUrl() {
     const url = `https://www.vinted.pl/api/v2/catalog/items?`;
-    const itemsPerPage = prompt("Items per page: ");
-    const catalogIds = prompt("Catalog IDs: ");
-    const colorIds = prompt("Color IDs: ");
-    const brandIds = prompt("Brand IDs: ");
-    const sizeIds = prompt("Size IDs: ");
-    const materialIds = prompt("Material IDs: ");
-    const videoGameRatingIds = prompt("Video game rating IDs: ");
-    const price = prompt("Price: ");
-    const currency = prompt("Currency: ");
-    const order = "newest_first";
-    return `${url}per_page=${itemsPerPage}&catalog_ids=${catalogIds}&color_ids=${colorIds}&brand_ids=${brandIds}&size_ids=${sizeIds}&material_ids=${materialIds}&video_game_rating_ids=${videoGameRatingIds}&price=${price}&currency=${currency}&order=${order}`;
+
+    const params = {
+        items_per_page: prompt("Items per page: "),
+        catalog_ids: prompt("Catalog IDs: "),
+        color_ids: prompt("Color IDs: "),
+        brand_ids: prompt("Brand IDs: "),
+        size_ids: prompt("Size IDs: "),
+        material_ids: prompt("Material IDs: "),
+        video_game_rating_ids: prompt("Video game rating IDs: "),
+        price: prompt("Price: "),
+        currency: prompt("Currency: "),
+        order: "newest_first",
+    };
+
+    for (const param of Object.keys(params)) {
+        params[param];
+    }
+    const urlParams = new URLSearchParams(params).toString();
+
+    return `${url}${urlParams}`;
+
+    // const itemsPerPage = prompt("Items per page: ");
+    // const catalogIds = prompt("Catalog IDs: ");
+    // const colorIds = prompt("Color IDs: ");
+    // const brandIds = prompt("Brand IDs: ");
+    // const sizeIds = prompt("Size IDs: ");
+    // const materialIds = prompt("Material IDs: ");
+    // const videoGameRatingIds = prompt("Video game rating IDs: ");
+    // const price = prompt("Price: ");
+    // const currency = prompt("Currency: ");
+    // const order = "newest_first";
+    // return `${url}items_per_page=${itemsPerPage}&catalog_ids=${catalogIds}&color_ids=${colorIds}&brand_ids=${brandIds}&size_ids=${sizeIds}&material_ids=${materialIds}&video_game_rating_ids=${videoGameRatingIds}&price=${price}&currency=${currency}&order=${order}`;
 }
 
 const dataUrl = parseUrl();
