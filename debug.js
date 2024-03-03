cc33a712b63d268d3a2d7f6a5f31e1071b10cd04
Your Name <londoyt@gmail.com>
2024-03-03 20:46:02 +0100

    debbug

--- debug.js
+++ debug.js
@@ -1,11 +1,21 @@
 const puppeteer = require("puppeteer");
 
-// get html from vinted website
-async function getHtml() {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto("https://www.vinted.pl/");
-    const html = await page.content();
-    return html;
+// get html from vinted website in console
+async function main() {
+    try {
+        const browser = await puppeteer.launch();
+        const [page] = await browser.pages();
+
+        await page.goto("https://www.vinted.pl/");
+        const data = await page.evaluate(
+            () => document.querySelector("*").outerHTML
+        );
+
+        console.log(data);
+
+        await browser.close();
+    } catch (err) {
+        console.error(err);
+    }
 }
-console.log(getHtml()); // debug
+main();
